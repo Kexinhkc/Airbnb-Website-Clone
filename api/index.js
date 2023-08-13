e0c1cf0e9714270997c0d9e83462ede38a38d8d4
apple <apple@Kexin.hub>
2023-08-14 01:52:58 +0930

    Moved server domain and port number to App.jsx, created server route to handle register requests, connected to Mongo database, moved Mongo credentials to an env file

--- api/index.js
+++ api/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config(); //Load environment variable from .env file
 const app = express();
+
+app.use(express.json());
 
 app.use(cors({
     credentials: true,
     origin:'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+// mongoose.connect('mongodb+srv://kexin:459000@cluster0.o3uceuw.mongodb.net/?retryWrites=true&w=majority;');
+
 app.get('/test',(req,res) => {
     res.json('test ok');
+    
 });
+
+app.post('/register',(req,res) => {
+    const { name,email,password } = req.body;
+    res.json({name, email, password});
+ });
 
 app.listen(4000);//Start an express server and makes it listen for incoming http requests at port 4000. Once having the server, it can make use of the routes you have defined above
